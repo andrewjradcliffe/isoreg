fcd11ffae5f43bebf09153d8541ae093fdf386b0
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2023-08-25 11:02:39 -0700

    Rough sketch of online isotonic regression

--- src/lib.rs
+++ src/lib.rs
@@ -429,4 +429,261 @@ mod tests {
         assert_eq!(-iso.interpolate(41.0), ((13.0 / 14.0) + 0.5) / 2.0);
         assert_eq!(-iso.interpolate(53.0), -(-0.4 + (0.4 / 4.0) * 1.0));
     }
+
+    #[test]
+    fn online_isoreg_with_dup_works_vikings_1() {
+        let (x, mut y) = vikings_data();
+        let z: Vec<f64> = vec![
+            22.0, 24.0, 26.0, 28.0, 29.0, 34.0, 36.0, 37.0, 39.0, 40.0, 42.0, 43.0, 45.0, 47.0,
+            48.0, 52.0, 56.0,
+        ];
+        let mu: Vec<f64> = vec![
+            1.0,
+            1.0,
+            1.0,
+            13.0 / 14.0,
+            13.0 / 14.0,
+            13.0 / 14.0,
+            13.0 / 14.0,
+            13.0 / 14.0,
+            13.0 / 14.0,
+            13.0 / 14.0,
+            0.5,
+            0.5,
+            0.5,
+            0.4,
+            0.4,
+            0.4,
+            0.0,
+        ];
+        y.iter_mut().for_each(|y_i| *y_i = -*y_i);
+
+        let iso = online_isoreg_with_dup(x, y);
+        for (z_i, mu_i) in z.into_iter().zip(mu.into_iter()) {
+            assert_eq!(-iso.interpolate(z_i), mu_i);
+        }
+        assert_eq!(-iso.interpolate(28.5), 13.0 / 14.0);
+        assert_eq!(-iso.interpolate(41.0), ((13.0 / 14.0) + 0.5) / 2.0);
+        assert_eq!(-iso.interpolate(53.0), -(-0.4 + (0.4 / 4.0) * 1.0));
+    }
+
+    #[test]
+    fn online_isoreg_with_dup_works_vikings_2() {
+        let (x, mut y) = vikings_data();
+        let z: Vec<f64> = vec![
+            22.0, 24.0, 26.0, 28.0, 29.0, 34.0, 36.0, 37.0, 39.0, 40.0, 42.0, 43.0, 45.0, 47.0,
+            48.0, 52.0, 56.0,
+        ];
+        let mu: Vec<f64> = vec![
+            1.0,
+            1.0,
+            1.0,
+            13.0 / 14.0,
+            13.0 / 14.0,
+            13.0 / 14.0,
+            13.0 / 14.0,
+            13.0 / 14.0,
+            13.0 / 14.0,
+            13.0 / 14.0,
+            0.5,
+            0.5,
+            0.5,
+            0.4,
+            0.4,
+            0.4,
+            0.0,
+        ];
+        y.iter_mut().for_each(|y_i| *y_i = -*y_i);
+
+        let mut iso = online_isoreg_with_dup(x[0..2].to_vec(), y[0..2].to_vec());
+        for i in 2..x.len() {
+            iso.add_point(x[i], y[i]);
+        }
+
+        for (z_i, mu_i) in z.into_iter().zip(mu.into_iter()) {
+            assert_eq!(-iso.interpolate(z_i), mu_i);
+        }
+        assert_eq!(-iso.interpolate(28.5), 13.0 / 14.0);
+        assert_eq!(-iso.interpolate(41.0), ((13.0 / 14.0) + 0.5) / 2.0);
+        assert_eq!(-iso.interpolate(53.0), -(-0.4 + (0.4 / 4.0) * 1.0));
+    }
+}
+
+#[derive(Debug)]
+pub struct OnlineIsoReg {
+    // Essential information to store
+    z: Vec<f64>,
+    v: Vec<f64>,
+    omega: Vec<usize>,
+    // The regression line
+    mu: Vec<f64>,
+    // Re-used temporaries
+    nu: Vec<f64>,
+    w: Vec<usize>,
+    u: Vec<usize>,
+}
+pub fn online_isoreg_with_dup(x: Vec<f64>, y: Vec<f64>) -> OnlineIsoReg {
+    // These two necessary conditions could be handled more delicately.
+    let n = y.len();
+    assert_eq!(x.len(), n);
+    assert!(n > 1);
+
+    let mut xy: Vec<_> = x.into_iter().zip(y.into_iter()).collect();
+    xy.sort_unstable_by(|(a, _), (b, _)| a.partial_cmp(b).unwrap());
+    let (x, y): (Vec<_>, Vec<_>) = xy.into_iter().unzip();
+
+    let (z, v, omega) = weighted_dup(&x, &y);
+    let n = z.len();
+
+    let mut nu: Vec<f64> = Vec::with_capacity(n);
+    nu.push(v[0]);
+    let mut w: Vec<usize> = Vec::with_capacity(n);
+    w.push(omega[0]);
+    let mut u: Vec<usize> = Vec::with_capacity(n);
+    u.push(1);
+    let mut j: usize = 0;
+    let mut i: usize = 1;
+    while i < n {
+        j += 1;
+        nu.push(v[i]);
+        w.push(omega[i]);
+        u.push(1);
+        i += 1;
+        while j > 0 && nu[j - 1] > nu[j] {
+            let w_prime = w[j - 1] + w[j];
+            let nu_prime = (w[j - 1] as f64 * nu[j - 1] + w[j] as f64 * nu[j]) / w_prime as f64;
+            nu[j - 1] = nu_prime;
+            w[j - 1] = w_prime;
+            u[j - 1] += u[j];
+            nu.pop();
+            w.pop();
+            u.pop();
+            j -= 1;
+        }
+    }
+    let mut mu_out: Vec<f64> = Vec::with_capacity(n);
+    // let mut pos: usize = 0;
+    let m = j + 1;
+    j = 0;
+    while j < m {
+        let mu = nu[j];
+        for _ in 0..u[j] {
+            // nu_out[pos] = mu;
+            // pos += 1
+            mu_out.push(mu);
+        }
+        j += 1;
+    }
+    OnlineIsoReg {
+        z,
+        v,
+        omega,
+        mu: mu_out,
+        nu,
+        w,
+        u,
+    }
+}
+
+impl OnlineIsoReg {
+    pub fn interpolate(&self, x: f64) -> f64 {
+        match self.z.binary_search_by(|z_j| z_j.partial_cmp(&x).unwrap()) {
+            Ok(j) => {
+                // An exact match on a binary search is inherently safe.
+                unsafe { self.mu.get_unchecked(j).clone() }
+            }
+            Err(j) => {
+                // We must determine where to interpolate from.
+                let k = self.z.len();
+                if j == 0 {
+                    self.mu[1]
+                        + (self.mu[0] - self.mu[1]) / (self.z[0] - self.z[1]) * (x - self.z[1])
+                } else if j == k {
+                    self.mu[k - 2]
+                        + (self.mu[k - 1] - self.mu[k - 2]) / (self.z[k - 1] - self.z[k - 2])
+                            * (x - self.z[k - 2])
+                } else {
+                    // x < z[j] => x - z[j] < 0
+                    self.mu[j - 1]
+                        + (self.mu[j] - self.mu[j - 1]) / (self.z[j] - self.z[j - 1])
+                            * (x - self.z[j - 1])
+                }
+            }
+        }
+    }
+    // pub fn x<'a>(&'a self) -> &'a Vec<f64> {
+    //     &self.x
+    // }
+    // pub fn mu<'a>(&'a self) -> &'a Vec<f64> {
+    //     &self.mu
+    // }
+    pub fn new() -> Self {
+        Self {
+            z: vec![],
+            v: vec![],
+            omega: vec![],
+            mu: vec![],
+            nu: vec![],
+            w: vec![],
+            u: vec![],
+        }
+    }
+    pub fn add_point(&mut self, x: f64, y: f64) {
+        match self.z.binary_search_by(|z_j| z_j.partial_cmp(&x).unwrap()) {
+            Ok(j) => {
+                let omega_prime = self.omega[j] + 1;
+                let v_prime = (self.omega[j] as f64 * self.v[j] + y) / omega_prime as f64;
+                self.v[j] = v_prime;
+                self.omega[j] = omega_prime;
+            }
+            Err(j) => {
+                self.v.insert(j, y);
+                self.omega.insert(j, 1);
+                self.z.insert(j, x);
+            }
+        }
+        self.isoreg_ordered_nodup();
+    }
+    fn isoreg_ordered_nodup(&mut self) {
+        self.nu.clear();
+        self.w.clear();
+        self.u.clear();
+        let n = self.z.len();
+
+        self.nu.push(self.v[0]);
+        self.w.push(self.omega[0]);
+        self.u.push(1);
+        let mut j: usize = 0;
+        let mut i: usize = 1;
+        while i < n {
+            j += 1;
+            self.nu.push(self.v[i]);
+            self.w.push(self.omega[i]);
+            self.u.push(1);
+            i += 1;
+            while j > 0 && self.nu[j - 1] > self.nu[j] {
+                let w_prime = self.w[j - 1] + self.w[j];
+                let nu_prime = (self.w[j - 1] as f64 * self.nu[j - 1]
+                    + self.w[j] as f64 * self.nu[j])
+                    / w_prime as f64;
+                self.nu[j - 1] = nu_prime;
+                self.w[j - 1] = w_prime;
+                self.u[j - 1] += self.u[j];
+                self.nu.pop();
+                self.w.pop();
+                self.u.pop();
+                j -= 1;
+            }
+        }
+        self.mu.clear();
+        let m = j + 1;
+        j = 0;
+        while j < m {
+            let mu = self.nu[j];
+            for _ in 0..self.u[j] {
+                self.mu.push(mu);
+            }
+            j += 1;
+        }
+    }
 }
